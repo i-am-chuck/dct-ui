338c0294b1f0e6c7ae109799979e316c747162ec
i-am-chuck <antony.caron.burdiak@gmail.com>
2023-03-10 14:33:30 +0100

    Clean and create simple button

---
+++ src/components/UiButton.vue
@@ -0,0 +1,41 @@
+<script>
+  export default {
+    name: 'UiButton',
+    props: {
+      label: String
+    }
+  }
+</script>
+
+<template>
+  <button>
+    {{ label }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+
+  $bg-color: #6d5dac;
+
+  button {
+    align-self: center;
+    border: 0;
+    background-color: $bg-color;
+    color: #ffffff;
+    padding: .9em 1.6em;
+    border-radius: 6px;
+    font-size: 16px;
+    font-weight: 500;
+    letter-spacing: .02em;
+    cursor: pointer;
+    transition: all .1s;
+
+    &:hover {
+      background-color: darken($bg-color, 10%);
+    }
+
+    &:active {
+      box-shadow: 0 0 0 8px rgba($bg-color, 20%);
+    }
+  }
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,25 @@
+<script>
+
+  import UiButton from "@/components/UiButton.vue";
+
+  export default {
+    name: 'Dashboard',
+    components: { UiButton }
+  }
+</script>
+
+<template>
+  <div id="dashboard">
+    <UiButton label="Button"/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+  #dashboard {
+    height: 100vh;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+  }
+</style>
